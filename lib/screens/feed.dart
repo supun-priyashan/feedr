715501d3e47fd2146986341598c6ad5815dcfe0d
DilithaR <ranjunadilitha@gmail.com>
2022-04-07 20:51:01 +0530

    feed updated

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,17 +1,83 @@
 import 'package:flutter/material.dart';
 
-class Feed extends StatefulWidget {
+class Feed extends StatelessWidget {
   const Feed({Key? key}) : super(key: key);
 
-  static const String routeName = '/feed';
-
-  @override
-  _FeedState createState() => _FeedState();
-}
-
-class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      theme: ThemeData.light(),
+      home: const FeedPage(),
+    );
+  }
+}
+
+class FeedPage extends StatefulWidget {
+  const FeedPage({Key? key}) : super(key: key);
+
+  @override
+  _FeedPageState createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  // Dummy Product Data Here
+  final List myProducts = List.generate(100, (index) {
+    return {"id": index, "title": "Product #$index", "price": index + 1};
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Kindacode.com'),
+        ),
+        body: ListView.builder(
+          itemCount: myProducts.length,
+          itemBuilder: (BuildContext ctx, index) {
+            // Display the list item
+            return Dismissible(
+              key: UniqueKey(),
+
+              // only allows the user swipe from right to left
+              direction: DismissDirection.endToStart,
+
+              // Remove this product from the list
+              // In production enviroment, you may want to send some request to delete it on server side
+              onDismissed: (_) {
+                setState(() {
+                  myProducts.removeAt(index);
+                });
+              },
+
+              // Display item's title, price...
+              child: Card(
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text(myProducts[index]["id"].toString()),
+                  ),
+                  title: Text(myProducts[index]["title"]),
+                  subtitle: Text("\$${myProducts[index]["price"].toString()}"),
+                  trailing: const Icon(Icons.arrow_back),
+                ),
+              ),
+
+              // This will show up when the user performs dismissal action
+              // It is a red background and a trash icon
+              background: Container(
+                color: Colors.red,
+                margin: const EdgeInsets.symmetric(horizontal: 15),
+                alignment: Alignment.centerRight,
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
