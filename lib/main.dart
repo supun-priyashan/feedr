e1e1d769bfb50d36910ed8602c5b8778833da342
Supun Priyashan <68734943+supun-priyashan@users.noreply.github.com>
2022-04-07 12:28:18 +0530

    Added sidenav Implemented rss retrieval

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:feedr/screens/subfeed.dart';
 import 'package:flutter/material.dart';
 import 'package:feedr/screens/login.dart';
 import 'package:feedr/screens/home.dart';
@@ -13,23 +14,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Feedr',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        // Define the default brightness and colors.
+        scaffoldBackgroundColor: Colors.black,
+        colorScheme: ColorScheme.dark(
+            primary: Colors.green,
+            onPrimary: Colors.white
+        ),
+
+        // Define the default font family.
+        fontFamily: 'Nexa',
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 18.0, fontFamily: 'Poppins'),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
       ),
       initialRoute: Home.routeName,
       routes: {
         Home.routeName: (context) => Home(),
         Login.routeName: (context) => Login(),
+        SubFeed.routeName: (context) => SubFeed(),
       },
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
-import 'package:feedr/screens/login.dart';
+import 'package:feedr/components/sidenav.dart';
 import 'package:flutter/material.dart';
+
+import 'package:feedr/screens/login.dart';
+import 'package:feedr/screens/feed.dart';
+import 'package:feedr/screens/subfeed.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,13 +20,15 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        backgroundColor: Colors.black,
       ),
+      drawer: const SideNav(),
       body: Container(
         child: Column(
           children: [
             ElevatedButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Login()));}, child: Text("login")),
-            ElevatedButton(onPressed: (){}, child: Text("gfdh")),
-            ElevatedButton(onPressed: (){}, child: Text("nbvn")),
+            ElevatedButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Feed()));}, child: Text("feed")),
+            ElevatedButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SubFeed()));}, child: Text("subfeed")),
           ],
         ),
       ),
